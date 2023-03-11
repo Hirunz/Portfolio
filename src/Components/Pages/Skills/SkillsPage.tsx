105d7fa1d713d682bc60dd8a1c123bcdd0725e92
hirunz <hirunz2000@gmail.com>
2023-03-11 21:13:08 +0530

    started skills page

--- src/Components/Pages/Skills/SkillsPage.tsx
+++ src/Components/Pages/Skills/SkillsPage.tsx
@@ -1,14 +1,52 @@
+import { Inbox } from '@mui/icons-material';
+import {
+  Divider,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from '@mui/material';
 import React from 'react';
 import { TestBR } from '../../Temp/ScrollTest';
 
 function Contact() {
   return (
-    <div id='skills'>
-      <h2>Skills Page</h2>
-      <p>Welcome to the skills page</p>
-      <TestBR />
+    <div
+      id='skills'
+      className='d-flex flex-column bg-dark'
+      style={{ paddingTop: '6vh' }}
+    >
+      <Typography> Skills Page</Typography>
+
+      <div className='bg-dark'>
+        <p>Welcome to the skills page</p>
+
+        <WebTechnologies />
+      </div>
     </div>
   );
 }
+
+const WebTechnologies = () => {
+  return (
+    <List>
+      {/* <ListItem disablePadding>
+        <ListItemIcon>
+          <Inbox />
+        </ListItemIcon>
+        <ListItemText>hello</ListItemText>
+      </ListItem>
+      <Divider />
+      <ListItem disablePadding>
+        <ListItemIcon>
+          <Inbox />
+        </ListItemIcon>
+        <ListItemText>hello</ListItemText>
+      </ListItem>
+      <Divider /> */}
+    </List>
+  );
+};
 
 export default Contact;
