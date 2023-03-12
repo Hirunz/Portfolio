ba823e84bb5159e3c4def03473c1c75d14f852c3
hirunz <hirunz2000@gmail.com>
2023-03-12 23:40:06 +0530

    changed background color to theme color.

--- src/Theme/Colours.tsx
+++ src/Theme/Colours.tsx
@@ -1,8 +1,8 @@
 const ThemeColors = {
   primary: '#0a9d90',
   secondary: '#087f90',
-  // background: '#100e17',
-  background: 'white',
+  background: '#100e17',
+  // background: 'white',
   error: '#a4332b',
   success: '#26852b',
   warning: '#e2a03f',
